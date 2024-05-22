9bd4c3a40563f004ee36ecab0a6ae8b3421324a2
ELIAS <elias.alasia.moreno@gmail.com>
2024-05-22 11:12:25 -0500

    actualizacion1

---
+++ src/components/Aside.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { CategoryList } from "./CategoryList"
+
+export const Aside = ({ data, setSelectedCategory })=>{
+    return(
+      <aside className="w-72 min-h-screen bg-gradient-to-t bg-sky-500 p-4 flex flex-col items-center gap-4">
+      <h1 className="text-white text-3xl font-semibold">
+        Meals
+      </h1>
+     
+
+        <CategoryList data={data} setSelectedCategory={setSelectedCategory}/>
+     
+      <div className="border border-red-500 h-20 w-full">
+
+      </div>
+    </aside>
+    )
+}
